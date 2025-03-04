ba5a83b124efe7afa1798170e5881f2eb0ae1c7f
momonong <morrischen2.9@gmail.com>
2025-03-04 14:13:01 +0800

    Add some test cases on the faq

--- src/main.py
+++ src/main.py
@@ -10,6 +10,9 @@ def main():
     # user_input = "Does Azure OpenAI support customer managed keys?"
     # 範例預設對話
     user_input = "復學證明可以什麼時候拿到"
+    # user_input = "how to drop out of school"
+    # user_input = "is there any scholarships for phd students"
+    # user_input = "how to apply for a scholarship"
     print("用戶:", user_input)
     answer = agent.chat(user_input)
     print("Agent 回答：", answer)
